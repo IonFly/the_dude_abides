8c8020c03b3a55663e561a76940bf543bf36c0b6
IonFly <spychalskyjc@gmail.com>
2018-10-28 12:59:57 -0400

    file to save edits to

---
+++ help_me_learn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#
+# File: help_me_learn.py
+# Abstract: I'd like some examples on how to code in Python
+
+# TODO: #1 init an integer
+
+# TODO: #2 print the int
+
+# TODO: #3 make a list
+
+# TODO: #4 Christmas is coming... check it twice?
+
+# TODO: #5 print a congratulatory message for the good kids
