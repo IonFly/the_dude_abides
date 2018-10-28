473e29a2fcd0014d7a8c784e215375a9a91e512a
LowLatency <dennis@datatech.ws>
2018-10-28 17:37:32 -0400

    Add solution.

---
+++ solution/sol1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+# File: help_me_learn.py
+# Abstract: I'd like some examples on how to code in Python
+
+variable = 100
+
+print("variable: " + str(variable))
+
+listOfChar = ['a', 'b', 'c', 'd']
+
+print("List of characters:" + str(listOfChar))
+print("Length of list: " + str(len(listOfChar)))
+
+for x in range(5):
+    for y in range(x):
+        print("#", end="")
+    print("")
+
+print("Christmas is about spending time with family and friends. It’s about creating happy memories that will last a lifetime. Merry Christmas to you and your family!")
+
